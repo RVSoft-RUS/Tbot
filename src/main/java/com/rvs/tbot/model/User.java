a076fae94cd874a4db59a7de2d6a6d7fb0970ddd
RVSoft-RUS <rovs15ds@gmail.com>
2022-04-26 22:17:15 +0300

    001

---
+++ src/main/java/com/rvs/tbot/model/User.java
@@ -0,0 +1,90 @@
+package com.rvs.tbot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long chatId;
+    private Long stateId;
+    private String phone;
+    private String email;
+    private Boolean admin;
+    private Boolean notified = false;
+
+    public User(Long chatId, Long stateId) {
+        this.chatId = chatId;
+        this.stateId = stateId;
+    }
+
+    public User(Long chatId, Long stateId, Boolean admin) {
+        this.chatId = chatId;
+        this.stateId = stateId;
+        this.admin = admin;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public Long getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(Long stateId) {
+        this.stateId = stateId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }
+
+    public Boolean getNotified() {
+        return notified;
+    }
+
+    public void setNotified(Boolean notified) {
+        this.notified = notified;
+    }
+}
